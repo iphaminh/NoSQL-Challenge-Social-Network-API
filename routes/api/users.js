d337c45165c8885ed68db49a5ef19273cda30c67
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-09-25 16:18:05 -0500

    updating friend in router user,js

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,18 +4,28 @@ const {
   getUserById,
   createUser,
   updateUser,
-  deleteUser
+  deleteUser,
+  addFriend,
+  removeFriend
 } = require('../../controllers/user-controller');
 
+// Set up routes for /api/users
 router
   .route('/')
   .get(getAllUsers)
   .post(createUser);
 
+// Set up routes for /api/users/:id
 router
   .route('/:id')
   .get(getUserById)
   .put(updateUser)
   .delete(deleteUser);
+
+// Set up routes for /api/users/:userId/friends/:friendId
+router
+  .route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend);
 
 module.exports = router;
